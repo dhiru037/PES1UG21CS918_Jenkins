8af1b434dba4db31d2cea6bf1d7dcada9d6bceca
Dhiruram B <93209461+dhiru037@users.noreply.github.com>
2024-03-06 13:58:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I have edited\n";
+  cout << "I have edited, pls work\n";
   return 0;
 }
